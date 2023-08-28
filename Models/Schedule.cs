16ba8f45e4694a8281ae4d557165645749dfd4b5
Nikita <niccotte404@gmail.com>
2023-08-29 00:21:09 +0300

    add Schedule and Date models

---
+++ Models/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentScheduleSystem.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Cabinet { get; set; }
+
+        [ForeignKey("Date")]
+        public Date Date { get; set; }
+        public string Time { get; set; }
+    }
+}
