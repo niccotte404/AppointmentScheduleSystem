637a761cdc436e4a66ba6d498430d1f3ea752873
Nikita <niccotte404@gmail.com>
2023-08-30 23:51:55 +0300

    add controller for company, emploee; view for company; fix css reload bug

---
+++ Controllers/EmploeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduleSystem.Controllers
+{
+    public class EmploeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
