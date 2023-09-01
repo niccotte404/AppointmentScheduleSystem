458f0315ec722cfaa844f54e5b706d89381df714
Nikita <niccotte404@gmail.com>
2023-09-01 21:07:27 +0300

    configure cloudinary api

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using AppointmentScheduleSystem.Data;
+using AppointmentScheduleSystem.Helpers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
