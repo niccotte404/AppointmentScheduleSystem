26fa5e18bd7beb10575135ca5043c304244236a9
Nikita <niccotte404@gmail.com>
2023-09-01 13:47:29 +0300

    add CloudinarySettings, install Cloudinary

---
+++ Helpers/CloudinarySettings.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet;
+
+namespace AppointmentScheduleSystem.Helpers
+{
+    public class CloudinarySettings
+    {
+        public string Account { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
