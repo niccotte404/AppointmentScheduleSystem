b6a7f1840a28d91ed4c93daeaff63e82fb8656a1
Nikita <niccotte404@gmail.com>
2023-09-04 19:11:00 +0300

    add create schedule view, add select option by enum, add simple day validation

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,4 +1,6 @@
-﻿using AppointmentScheduleSystem.ViewModels;
+﻿using AppointmentScheduleSystem.Data;
+using AppointmentScheduleSystem.Data.Enum;
+using AppointmentScheduleSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppointmentScheduleSystem.Controllers
@@ -19,6 +21,24 @@ namespace AppointmentScheduleSystem.Controllers
         public async Task<IActionResult> Create(CreateScheduleViewModel createScheduleViewModel)
         {
             return View();
+        }
+
+        private async Task<bool> IsDayValid(int day, string month, int year)
+        {
+            IEnumerable<string> months = new[] { "January", "March", "May", "July", "August", "October", "December" };
+            if (day < 1 || day > 31 || year < 2023)
+            {
+                return false;
+            }
+            else if (day > 30 && months.FirstOrDefault(elem => elem == month) == null)
+            {
+                return false;
+            }
+            else if (day > 28 && month == "February")
+            {
+                return false;
+            }
+            return true;
         }
 
     }
---
+++ Data/Enum/Months.cs
@@ -0,0 +1,18 @@
+﻿namespace AppointmentScheduleSystem.Data.Enum
+{
+    public enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        Decemper
+    }
+}
